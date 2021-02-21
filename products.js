85c29a6a5d0f4a0f6a78e547fd81c8b4a1cf3e66
Jasraj Bhatia <j.b60980@gmail.com>
2021-02-21 09:48:23 +0400

    products.js updated

---
+++ products.js
@@ -0,0 +1,29 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresistible</em>," + "organic goods for your cat.",
+        "price": 2.00,
+        "image": "images/cat-eating.jpeg",
+        "AvailableInventory":5,
+        "rating": 2
+    },
+    {
+        "id": 1002,
+        "title": "Yarn",
+        "description": "Yarn your cat can play with for a very <strong>long</strong> tome!",
+        "price": 2.99,
+        "image": "images/yarn.jpeg",
+        "AvailableInventory":5,
+        "rating": 3  
+    },
+    {
+        "id": 1003,
+        "title": "Yarn",
+        "description": "Yarn your cat can play with for a very <strong>long</strong> tome!",
+        "price": 8.95,
+        "image": "images/yarn.jpeg",
+        "AvailableInventory":5,
+        "rating": 3  
+    },
+]
