5ec18107f84882da280911fee3a2cc30c9d656c9
Jasraj Bhatia <j.b60980@gmail.com>
2021-02-21 09:32:39 +0400

    service-worker.js file added

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'products.js',
+    'petstor.webmanifest',
+    'images/yarn.jpeg',
+    'images/cat-eating.jpeg',
+    'images/icon-512.webp',
+];
