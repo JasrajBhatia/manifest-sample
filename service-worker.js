44aa063a6fd1984228630110ed287f8f117185d3
Jasraj Bhatia <j.b60980@gmail.com>
2021-02-21 09:44:26 +0400

    Event Listener added

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,18 @@ var cacheName = 'petstore-v1';
 var cacheFiles = [
     'index.html',
     'products.js',
-    'petstor.webmanifest',
+    'petstore.webmanifest',
     'images/yarn.jpeg',
     'images/cat-eating.jpeg',
     'images/icon-512.png',
 ];
+
+self.addEventListener('install', (e) => {
+    console.log('[(Service Worker] Installed');
+    e.waitUntil(
+        caches.open(cacheName).this((cache) => {
+            console.log('[(Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles)
+        })
+    );
+});
